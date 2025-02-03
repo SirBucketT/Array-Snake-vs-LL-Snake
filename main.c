87834976358e6be038223639f4f1788fcb1ec7e2
SirBucketT <tkpriva@icloud.com>
2025-02-03 13:24:28 +0100

    update

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include "header.h"
+#include "raylib.h"
+
+#define SCREEN_WIDTH   1800
+#define SCREEN_HEIGHT  900
 
 int main(void) {
-    printf("Hello, World!\n");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake.. Snake.. SNAKE!");
+
+    while (!WindowShouldClose()){
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+    }
+    EndDrawing();
+    CloseWindow();
     return 0;
 }
