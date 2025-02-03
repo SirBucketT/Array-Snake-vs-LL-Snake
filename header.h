5214078285ed0833cac19a27c01bfc9fb2ba238e
SirBucketT <tkpriva@icloud.com>
2025-02-03 13:14:26 +0100

    updated with header

---
+++ header.h
@@ -0,0 +1,8 @@
+//
+// Created by theodor on 2025-02-03.
+//
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#endif //HEADER_H
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "header.h"
 
 int main(void) {
     printf("Hello, World!\n");
