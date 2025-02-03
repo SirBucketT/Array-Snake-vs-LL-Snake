00f42d5856aae40cc366c5456c276e312c6545c7
SirBucketT <tkpriva@icloud.com>
2025-02-03 13:58:08 +0100

    update

--- header.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by theodor on 2025-02-03.
-//
-
-#ifndef HEADER_H
-#define HEADER_H
-
-#endif //HEADER_H
--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
-#include "header.h"
+#include <stdlib.h>
+#include <stdbool.h>
 #include "raylib.h"
-#include "external/glfw/src/internal.h"
 
 #define SCREEN_WIDTH   1800
 #define SCREEN_HEIGHT  900
+
+struct Color green = {173, 204, 96, 255};
+struct Color darkGreen = {43, 51, 24, 255};
+
+struct Circle {
+    Vector2 center;
+    float radius;
+};
+
+typedef struct {
+    struct Circle shape;
+    bool active;
+    Color darkGreen;
+} Food;
 
 int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake.. Snake.. SNAKE!");
@@ -12,10 +26,10 @@ int main(void) {
     while (!WindowShouldClose()){
 
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(green);
 
+        EndDrawing();
     }
-    EndDrawing();
     CloseWindow();
     return 0;
 }
